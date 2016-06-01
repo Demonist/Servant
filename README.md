15940fee07b7255a6215ed26bef0bb0c82cf8095
Demonist <Demonist616@gmail.com>
2016-06-02 02:17:37 +0300

    2.0 Init commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+#Servant
+It is the World Of Warcraft addon that play some sound when you level up, die, complete a quest or leave from a combat.  
+It is a updated version of 'Servant' addon. The original addon was writed by Andrew O'Hara in 2009.
+
+The original addon site: [http://wow.curseforge.com/addons/servant-lich-king/](http://wow.curseforge.com/addons/servant-lich-king/)
+
+#Installation
+Just copy **Servant** folder to **World of Warcraft\\Interface\\AddOns\\**.
+
+#Have a question?
+Write - **demonist616@gmail.com**
---
+++ Servant/Servant.lua
@@ -0,0 +1,73 @@
+﻿-- It is a updated version of 'Servant' addon
+-- The original addon was writed by Andrew O'Hara in 2009.
+
+----------------------------
+-- Sounds to play on kill --
+----------------------------
+local killsounds = {
+	"EH_LichKing_Chapter1Whisper13.ogg",
+	"EH_LichKing_Chapter1Whisper01.ogg",
+	"EH_LichKing_Chapter1Whisper04.ogg",
+	"EH_LichKing_Chapter3Shout03.ogg",
+	"EH_LichKing_Chapter1Whisper03.ogg",
+	"EH_LichKing_Chapter1Whisper05.ogg",
+	"EH_LichKing_Chapter1Whisper12.ogg",
+	"EH_LichKing_Chapter1Whisper13.ogg"
+}
+	
+--------------------------------------
+-- Sounds to play on Quest Complete --
+--------------------------------------
+local achievsounds = {
+	"EH_LichKing_Greeting2.ogg",
+	"EH_LichKing_Chapter3Shout13.ogg",
+	"EH_LichKing_Farewell3.ogg",
+	"EH_LichKing_Farewell4.ogg",
+	"EH_LichKing_Chapter3Shout02.ogg"
+}	
+
+--------------------------------
+-- Sounds to play on level Up --
+--------------------------------
+local lvlsounds = { 
+	"EH_LichKing_Farewell1.ogg",
+	"EH_LichKing_Farewell2.ogg",
+	"EH_LichKing_Greeting4.ogg",
+	"EH_LichKing_Chapter3Shout16.ogg"
+}	
+
+-----------------------------
+-- Sounds to play on death --
+-----------------------------
+local deathsounds = {
+	"EH_LichKing_Chapter1Whisper08.ogg",
+	"EH_LichKing_Chapter1Whisper07.ogg",
+	"EH_LichKing_Chapter1Whisper09.ogg",
+	"EH_LichKing_Chapter1Whisper15.ogg",
+	"EH_LichKing_Chapter1Whisper16.ogg"
+}	
+
+local time = GetTime()
+	
+local function OnEvent(self, event)	
+	if GetTime() > time + 3 then
+		if event == "PLAYER_LEVEL_UP" then
+			PlaySoundFile("Sound\\Creature\\LichKing\\"..lvlsounds[math.random(#lvlsounds)])
+		elseif event == "ACHIEVEMENT_EARNED" then
+			PlaySoundFile("Sound\\Creature\\LichKing\\"..achievsounds[math.random(#achievsounds)])
+		elseif event == "PLAYER_DEAD" then
+			PlaySoundFile("Sound\\Creature\\LichKing\\"..deathsounds[math.random(#deathsounds)])
+		elseif event == "PLAYER_REGEN_ENABLED" and (math.random(4)) == 1 then
+			PlaySoundFile("Sound\\Creature\\LichKing\\"..killsounds[math.random(#killsounds)])
+		end
+		
+		time = GetTime()
+	end
+end
+
+local eventFrame = CreateFrame("Frame")
+eventFrame:SetScript("OnEvent", OnEvent)
+eventFrame:RegisterEvent("PLAYER_LEVEL_UP")
+eventFrame:RegisterEvent("ACHIEVEMENT_EARNED")
+eventFrame:RegisterEvent("PLAYER_DEAD")
+eventFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
